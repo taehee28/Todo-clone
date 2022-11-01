22b5f96f19946a0faa2f14f8d34cd390017a148f
Kang Taehee <dev.thk28@gmail.com>
2022-11-01 17:23:27 +0900

    Dependency 설정

--- app/src/main/java/com/thk/todo_clone/MainActivity.kt
+++ app/src/main/java/com/thk/todo_clone/MainActivity.kt
@@ -17,25 +17,8 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             TodocloneTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
+
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    TodocloneTheme {
-        Greeting("Android")
     }
 }
