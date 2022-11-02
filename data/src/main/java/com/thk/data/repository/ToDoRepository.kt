5d9888d6630a11d52d2836e10ff7b1e5a309e78d
Kang Taehee <dev.thk28@gmail.com>
2022-11-02 17:49:57 +0900

    ToDoRepository 작성

---
+++ data/src/main/java/com/thk/data/repository/ToDoRepository.kt
@@ -0,0 +1,40 @@
+package com.thk.data.repository
+
+import com.thk.data.database.ToDoDao
+import com.thk.data.models.ToDoTask
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+interface ToDoRepository {
+    fun getAllTasks(): Flow<List<ToDoTask>>
+    fun getTasksSortByLowPriority(): Flow<List<ToDoTask>>
+    fun getTasksSortByHighPriority(): Flow<List<ToDoTask>>
+    fun getSelectedTask(taskId: Int): Flow<ToDoTask>
+    suspend fun addTask(toDoTask: ToDoTask)
+    suspend fun updateTask(toDoTask: ToDoTask)
+    suspend fun deleteTask(toDoTask: ToDoTask)
+    suspend fun deleteAllTasks()
+    fun searchDatabase(searchQuery: String): Flow<List<ToDoTask>>
+}
+
+class ToDoRepositoryImpl @Inject constructor(
+    private val toDoDao: ToDoDao
+) : ToDoRepository {
+    override fun getAllTasks() = toDoDao.getAllTasks()
+
+    override fun getTasksSortByLowPriority() = toDoDao.sortByLowPriority()
+
+    override fun getTasksSortByHighPriority() = toDoDao.sortByHighPriority()
+
+    override fun getSelectedTask(taskId: Int) = toDoDao.getSelectedTask(taskId)
+
+    override suspend fun addTask(toDoTask: ToDoTask) = toDoDao.addTask(toDoTask)
+
+    override suspend fun updateTask(toDoTask: ToDoTask) = toDoDao.updateTask(toDoTask)
+
+    override suspend fun deleteTask(toDoTask: ToDoTask) = toDoDao.deleteTask(toDoTask)
+
+    override suspend fun deleteAllTasks() = toDoDao.deleteAllTasks()
+
+    override fun searchDatabase(searchQuery: String) = toDoDao.searchDatabase(searchQuery)
+}
