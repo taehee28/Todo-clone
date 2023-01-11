0f6b599d37dc2f6eaea89c30869af452372c8e68
taehee28 <dev.thk28@gmail.com>
2023-01-11 21:41:08 +0900

    Splash 화면 아이콘 애니메이션 적용

--- app/src/main/java/com/thk/todo_clone/ui/screens/splash/SplashScreen.kt
+++ app/src/main/java/com/thk/todo_clone/ui/screens/splash/SplashScreen.kt
@@ -2,18 +2,18 @@ package com.thk.todo_clone.ui.screens.splash
 
 import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.window.SplashScreen
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.res.painterResource
@@ -29,7 +29,18 @@ import kotlinx.coroutines.delay
 fun SplashScreen(
     navigateToListScreen: () -> Unit
 ) {
+    var startAnimation by remember { mutableStateOf(false) }
+    val offsetState by animateDpAsState(
+        targetValue = if (startAnimation) 0.dp else (-100).dp,
+        animationSpec = tween(durationMillis = 1000)
+    )
+    val alphaState by animateFloatAsState(
+        targetValue = if (startAnimation) 1f else 0f,
+        animationSpec = tween(durationMillis = 1000)
+    )
+
     LaunchedEffect(key1 = Unit) {
+        startAnimation = true
         delay(3000)
         navigateToListScreen()
     }
@@ -44,7 +55,10 @@ fun SplashScreen(
             painter = painterResource(id = R.drawable.ic_check_circle),
             contentDescription = stringResource(id = R.string.to_do_logo),
             colorFilter = ColorFilter.tint(Color.White),
-            modifier = Modifier.size(100.dp)
+            modifier = Modifier
+                .size(100.dp)
+                .offset(y = offsetState)
+                .alpha(alpha = alphaState)
         )
     }
 }
