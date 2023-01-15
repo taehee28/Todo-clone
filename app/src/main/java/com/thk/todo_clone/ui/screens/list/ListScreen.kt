f8ab449ecf375a935c6c0e1e2419449eb34fc1ef
taehee28 <dev.thk28@gmail.com>
2023-01-15 16:18:31 +0900

    Update: 항목 여러개 삭제 시 이전에 나타난 SnackBar가 dismiss하도록 변경

--- app/src/main/java/com/thk/todo_clone/ui/screens/list/ListScreen.kt
+++ app/src/main/java/com/thk/todo_clone/ui/screens/list/ListScreen.kt
@@ -74,6 +74,7 @@ fun ListScreen(
                 onSwipeToDelete = { action, task ->
                     sharedViewModel.action.value = action
                     sharedViewModel.updateTaskFields(selectedTask = task)
+                    scaffoldState.snackbarHostState.currentSnackbarData?.dismiss()
                 },
                 navigateToTaskScreen = navigateToTaskScreen
             )
